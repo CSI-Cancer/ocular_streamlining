f45504a8cffdcf1b611a60fd89f84e18d72f2993
neednoname09 <mandyana@usc.edu>
2024-11-04 10:08:45 -0800

    Data preprocessing

--- streamlining_training/dataset.py
+++ streamlining_training/dataset.py
@@ -4,6 +4,7 @@ import sys
 
 from loguru import logger
 from tqdm import tqdm
+import pyreadr
 
 import pandas as pd
 import numpy as np
@@ -13,13 +14,16 @@ from sklearn.model_selection import train_test_split
 import itertools
 import glob
 import json
+from itertools import permutations
 
 # Add the project root directory to sys.path
 sys.path.append(str(Path(__file__).resolve().parents[1]))
 
 from csi_utils import databases
 from streamlining_training.config import (
-    RAW_DATA_DIR, COHORT_PAIR, INTERIM_DATA_DIR, CUSTOM_SPLIT
+    RAW_DATA_DIR, COHORT_PAIR, INTERIM_DATA_DIR, CUSTOM_SPLIT, 
+    PRODUCTION_REPORT_DIR, PROCESSED_DATA_DIR, ocular_params,
+    channel_map, feature_sdom_levels
 )
 
 def remove_suffix(slide_id, marker='_'):
@@ -61,12 +65,11 @@ def get_events_and_perML(
                                         write_protect=True)
     # Query for tube ids for all the slides
     slide_ids = generate_friendly_list(slide_ids["slide_id"].apply(remove_suffix).values.tolist())
-
     data_from_db = {key: None for key in both}
 
     if "per_ml" in both:
         # Query for slide and tube
-        query = ["select slide_id, tube_id from slide where slide_id = (%s)"]*len(slide_ids)
+        query = ["select slide_id, tube_id from slide where slide_id = %s"]*len(slide_ids)
         query_result = handler.get(query, slide_ids)
         slides_tubes = pd.DataFrame(list(itertools.chain.from_iterable(query_result['results'])),
                                 columns=query_result['headers'][0] )
@@ -183,10 +186,293 @@ def get_manual_events(slides_cohort, classes):
     per_ml = pd.concat(per_ml_df)
     return per_ml, events
 
+def get_data_from_files(slide_id: str,
+                         ocular_params: dict,
+                         ocular_path: Path = PRODUCTION_REPORT_DIR):  
+    file_holder = {}
+    if ocular_params["dapipos"]:
+        event_files = ocular_params["dapi_pos_files"]
+    else:
+        event_files = ocular_params["dapi_neg_files"]
+    
+    failed_loads = []
+    empty_files = []
+
+    for file in event_files:
+        file_path = ocular_path / slide_id / "ocular" / file
+
+        try:
+            file_holder[file] = pyreadr.read_r(file_path)
+        except Exception as e:
+            logger.error(f"Traceback while loading files for the slide : {slide_id} \n {e}")
+            failed_loads.append(file)
+            continue
+        file_holder[file] = file_holder[file][None]
+        if (file == "ocular_interesting.rds" or file == "ocular_not_interesting.rds"
+            ) and ocular_params["drop_common_cells"]:
+            file_holder[file] = file_holder[file][
+                file_holder[file]["catalogue_classification"] != "common_cell"]
+        if len(file_holder[file]) == 0:
+            empty_files.append(file)
+        else:
+            if ocular_params["dapipos"]:
+                file_holder[file]["rowname"] = file_holder[file]["rowname"].astype(str)
+                split_res = file_holder[file]["rowname"].str.split(" ", n=1, expand=True)
+
+                if len(split_res.columns) != 2:
+                        logger.error(f"Slide {slide_id} rowname column has issues")
+                file_holder[file][["frame_id", "cell_id"]] = split_res
+                file_holder[file][
+                        ["frame_id", "cell_id"]
+                ] = file_holder[file][["frame_id", "cell_id"]].astype(
+                    "int"
+                )
+            file_holder[file].reset_index(drop=True, inplace=True)
+
+    for empty in empty_files:
+        logger.info(f"deleting empty file: {empty} for the slide: {slide_id}")
+        del file_holder[empty]
+    
+    for dailed in failed_loads:
+        logger.error(f"Deleting failed to load file: {failed_loads} for the slide: {slide_id}")
+        del file_holder[failed_loads]
+    
+    ocular_data = [
+            file_holder[filename]
+            for filename in file_holder.keys()
+        ]
+    if len(ocular_data) != 0:
+        ocular_data = pd.concat(ocular_data)
+        logger.info(f"Found {len(ocular_data)} events for slide {slide_id} in ocular files")
+
+    return ocular_data
+
+def get_frame_morphs(slide_id, ocular_params,
+                     path: Path = PRODUCTION_REPORT_DIR):
+
+    slide__path = path / slide_id / "ocular" 
+    morph_files = ["framestat-means.csv", "framestat-dev.csv"]
+    prefixes = ["means", "dev"]
+    files = {}
+    for file, prefix in zip(morph_files, prefixes):
+        file_path = slide__path / file
+        df = pd.read_csv(file_path)
+        df = df.rename(columns={df.columns[0]: 'frame_id'})
+        df = df.rename(columns={col: f"frame_{prefix}_{col}" for col in df.columns[1:]})
+        files[prefix] = df
+
+    frame_stats = pd.merge(files["means"], files["dev"], on="frame_id")
+    return frame_stats
+
+def get_slide_stats(slide_id, ocular_params,
+                    path: Path = PRODUCTION_REPORT_DIR):
+    slide__path = path / slide_id / "ocular"
+    slide_stat_df = pd.read_csv(slide__path / "slidestat-calc.csv")
+    # Index 0 is the standard deviation
+    slide_dev = slide_stat_df.iloc[1].to_frame().transpose()
+    slide_dev.reset_index(inplace=True)
+    # Index 0 is the mean
+    slide_mean = slide_stat_df.iloc[0].to_frame().transpose()
+    slide_mean.reset_index(inplace=True)
+    slide_dev.drop(columns=["Unnamed: 0"], inplace=True)
+    slide_mean.drop(columns=["Unnamed: 0"], inplace=True)
+    slide_dev = slide_dev.add_prefix("slide_dev_")
+    slide_mean = slide_mean.add_prefix("slide_means_")\
+    
+    slide_stats = pd.concat([slide_mean, slide_dev], axis=1)
+    return slide_stats
+
+def calculate_sdom(df, feature, prefix):
+    feature_mean = prefix + "_means_" + feature
+    feature_sd = prefix + "_dev_" + feature
+    df[prefix+"_sdom_"+feature] = df[feature] - df[feature_mean]
+    df[prefix+"_sdom_"+feature] = df[prefix+"_sdom_"+feature].divide(df[feature_sd])
+    df[prefix+"_sdom_"+feature] = df[prefix+"_sdom_"+feature].replace([np.inf, -np.inf], 0)
+    if df[prefix+"_sdom_"+feature].isna().any():
+        n_of_nas = df[prefix+"_sdom_"+feature][df[prefix+"_sdom_"+feature].isna()].index
+        logger.info(
+            f"Replacing nan with zero for {prefix}_sdom_{feature} , number of entries: {len(n_of_nas)}"
+        )
+        df[prefix+"_sdom_"+feature].fillna(0, inplace=True)
+    return df[prefix+"_sdom_"+feature]
+
+def generate_sdom(df, feature_sdom_levels, compute_features):
+    for prefix in feature_sdom_levels.keys():
+        for feature in feature_sdom_levels[prefix]:
+            df[prefix+"_sdom_"+feature] = calculate_sdom(df, feature, prefix)
+    return df
+
+
+def get_top_level_data(slide_id, per_ml):
+    df = per_ml[per_ml["slide_id"] == slide_id].copy()
+    if len(df) == 0:
+        logger.info(f"No top level data found for the slide: {slide_id}")
+        return None
+    tube_id = df["tube_id"].values[0]
+    patient_id = df["patient_id"].values[0]
+    dapi_count = df["dapi_count_ocular"].values[0]
+    wbc_count = df["wbc_count"].values[0]
+    conversion_factor = wbc_count / dapi_count
+    clinical_classification = df["classification"].values[0]
+    top_data = pd.DataFrame([{
+        "slide_id": slide_id,
+        "tube_id": tube_id,
+        "patient_id": patient_id,
+        "dapi_count": dapi_count,
+        "wbc_count": wbc_count,
+        "conversion_factor": conversion_factor,
+        "clinical_classification": clinical_classification
+    }])
+    return top_data
+
+def merge_all(uncurated_events,
+              frame_stats,
+              slide_stats,):
+    rows = len(frame_stats)
+    slide_stats = pd.concat(
+            [slide_stats] * rows,
+            ignore_index=True,
+        )
+    final_df = frame_stats.merge(
+            slide_stats, left_index=True, right_index=True, how="inner"
+        )
+    final_df = pd.merge(uncurated_events, final_df, on=["frame_id"])
+
+    return final_df
+
+
+def process_slide(slide_id,
+                     events,
+                       per_ml,
+                        ocular_params,):
+    # Slice events corresponding to the slide
+    manual_interesting = events[(events['slide_id'] == slide_id) & (events['interesting'] == 1)].copy()
+    manual_not_interesting = events[(events['slide_id'] == slide_id) & (events['interesting'] == 0)].copy()
+    manual_interesting["unique_id"] = (
+        manual_interesting["slide_id"].astype(str) + "_" +
+        manual_interesting["frame_id"].astype(str) + "_" +
+        manual_interesting["cell_id"].astype(str)
+        )
+    manual_not_interesting["unique_id"] = (
+        manual_not_interesting["slide_id"].astype(str) + "_" +
+        manual_not_interesting["frame_id"].astype(str) + "_" +
+        manual_not_interesting["cell_id"].astype(str)
+        )
+    top_data = get_top_level_data(slide_id, per_ml)
+
+    uncurated_events = get_data_from_files(slide_id, ocular_params)
+    if len(uncurated_events) != 0:
+        uncurated_events["slide_id"] = slide_id
+
+    uncurated_events["unique_id"] = (
+        uncurated_events["slide_id"].astype(str) + "_" +
+        uncurated_events["frame_id"].astype(str) + "_" +
+        uncurated_events["cellx"].astype(str) + "_" +
+        uncurated_events["celly"].astype(str)
+    )
+    uncurated_events.sort_values(by=["cell_id"],
+                                  ascending=True, inplace=True)
+    uncurated_events.drop_duplicates(subset=['unique_id'],
+                                      keep='first', inplace=True)
+    uncurated_events["unique_id"] = (
+        uncurated_events["slide_id"].astype(str) + "_" +
+        uncurated_events["frame_id"].astype(str) + "_" +
+        uncurated_events["cell_id"].astype(str)
+    )
+    uncurated_events.drop_duplicates(subset=['unique_id'],
+                                      keep='first', inplace=True)
+    
+    frame_stats = get_frame_morphs(slide_id, ocular_params)
+
+    slide_stats = get_slide_stats(slide_id, ocular_params)
+
+    uncurated_events = merge_all(uncurated_events,
+                         frame_stats,
+                         slide_stats)
+    if ocular_params['drop_columns']:
+        columns_to_keep = uncurated_events.columns
+        if 'haralick' in ocular_params['drop_morphs']:
+            columns_to_keep = [col for col in columns_to_keep if '.h.' not in col]
+        if 'theta' in ocular_params['drop_morphs']:
+            columns_to_keep = [col for col in columns_to_keep if 'theta' not in col]
+            
+        if 'blurred' in ocular_params['drop_morphs']:
+            for channel in channel_map.keys():
+                columns_to_keep = [col for col in columns_to_keep if 'B'+channel_map[channel] not in col]
+
+        if 'multi_channel' in ocular_params['drop_morphs']:
+            drop_keys = [''.join(pair) for pair in permutations(channel_map.values(), 2)]
+            for key in drop_keys:
+                columns_to_keep = [col for col in columns_to_keep if key not in col]
+
+        if 'mean_sd_q05' in ocular_params['drop_morphs']:
+            columns_to_keep=[col for col in columns_to_keep if ('.mean' in col) or ('.sd' in col) or ('.q05' in col) ]
+        
+        if 'mean_q05' in ocular_params['drop_morphs']:
+            columns_to_keep=[col for col in columns_to_keep if ('.mean' in col) or ('.q05' in col) ]
+        
+        if 'remove_slide_level' in ocular_params['drop_morphs']:
+            columns_to_keep=[col for col in columns_to_keep if 'slide' not in col ]
+        
+        if 'remove_frame_level' in ocular_params ['drop_morphs']:
+            columns_to_keep=[col for col in columns_to_keep if 'frame' not in col ]
+
+        # Adding the frame and slide id since it was dropped during morphs drop
+        columns_to_keep+=["frame_id", "slide_id"]
+        compute_features = []
+        for prefix in feature_sdom_levels.keys():
+            compute_features += [prefix +"_sdom_"+item for item in feature_sdom_levels[prefix]]
+        columns_to_keep += compute_features
+
+        uncurated_events = generate_sdom(uncurated_events, feature_sdom_levels, compute_features)
+
+
+        uncurated_events = uncurated_events[columns_to_keep]
+
+        uncurated_interesting = uncurated_events[(
+        uncurated_events['unique_id'].isin(manual_interesting['unique_id']) )].copy()
+
+        uncurated_not_interesting = uncurated_events[(
+        ~(uncurated_events['unique_id'].isin(manual_interesting['unique_id'])) )]
+
+        manual_not_interesting = uncurated_not_interesting[uncurated_not_interesting['unique_id'].isin(
+                                                            manual_not_interesting['unique_id'])]
+        
+        uncurated_not_interesting = uncurated_not_interesting[~(uncurated_not_interesting['unique_id'].isin(
+                                                            manual_not_interesting['unique_id']))]
+
+        if ocular_params['not_int_sample_size'] > 0:
+            if len(manual_not_interesting) > ocular_params['not_int_sample_size']:
+                manual_not_interesting=manual_not_interesting.sample(n=ocular_params['not_int_sample_size'],
+                                                                 random_state=42)
+        uncurated_interesting["interesting"] = 1
+        uncurated_not_interesting["interesting"] = 0
+        manual_not_interesting["interesting"] = 0
+
+        final_df = pd.concat([uncurated_interesting, uncurated_not_interesting,
+                              manual_not_interesting])
+        return final_df
+        
+
+def get_slides_data(slide_ids: list,
+                    events: pd.DataFrame,
+                    per_ml: pd.DataFrame,
+                    ocular_params: dict = ocular_params,
+                    ):
+    slide_data = []
+    for slide_id in slide_ids:
+        slide_data.append(process_slide(slide_id = slide_id,
+                                    events = events,
+                                    per_ml = per_ml,
+                                    ocular_params=ocular_params,))
+    slide_data = pd.concat(slide_data, ignore_index=True)
+    return slide_data
+
 def main(
     # ---- REPLACE DEFAULT PATHS AS APPROPRIATE ----
     input_path: Path = RAW_DATA_DIR ,
     output_path: Path = INTERIM_DATA_DIR,
+    processed_path: Path = PROCESSED_DATA_DIR,
     cohort: list = COHORT_PAIR,
     split: dict = CUSTOM_SPLIT,
     # ----------------------------------------------
@@ -216,11 +502,13 @@ def main(
     events["slide_id"] = events["slide_id"].apply(add_suffix)
     per_ml["slide_id"] = per_ml["slide_id"].apply(add_suffix)
     logger.info(f"Saving data to excel files in {output_path} folder.")
-    events.to_excel(output_path / "events.xlsx", index=False)
-    per_ml.to_excel(output_path / "per_ml.xlsx", index=False)
+    events.to_csv(output_path / "events.csv", index=False)
+    per_ml.to_csv(output_path / "per_ml.csv", index=False)
+    logger.info(f"Found {len(events)} events in {len(slides_cohort)} slides")
 
+    # List out all the processed slides
     slides_cohort = list(set(per_ml["slide_id"]))
-
+    # Split the slides into train, val and test
     if split:
         train_slides = per_ml[(
             per_ml['classification'].isin(split['train'])
@@ -237,13 +525,23 @@ def main(
                                                      random_state=43,
                                                      test_size=0.2)
     # Save the train, val and test slides
+    data = {}
     with open(f'{output_path}/train_slides.json', 'w') as f:
         json.dump(train_slides, f)
+        data["train"] = train_slides
     with open(f'{output_path}/val_slides.json', 'w') as f:
         json.dump(val_slides, f)
+        data["val"] = val_slides
     with open(f'{output_path}/test_slides.json', 'w') as f:
         json.dump(test_slides, f)
+        data["test"] = test_slides
     
+    for key, value in data.items():
+        logger.info(f"processing {key} slides")
+        slides = get_slides_data(slide_ids = value,
+                                events = events,
+                                per_ml = per_ml)
+        slides.to_csv(PROCESSED_DATA_DIR / f"{key}_data.csv", index=False)
     logger.success("Processing dataset complete.")
     # -----------------------------------------
 
--- streamlining_training/features.py
+++ streamlining_training/features.py
@@ -1,29 +1,44 @@
 from pathlib import Path
+import sys
 
-import typer
 from loguru import logger
 from tqdm import tqdm
 
-from streamlining_training.config import PROCESSED_DATA_DIR
+# ML packages
+import pandas as pd
+import numpy as np
+from sklearn.ensemble import HistGradientBoostingClassifier
+from imblearn.over_sampling import RandomOverSampler
+from sklearn.inspection import permutation_importance
 
-app = typer.Typer()
+# Visualizations packages
+import matplotlib.pyplot as plt
+import seaborn as sns
+from scipy.cluster import hierarchy
+from scipy.spatial.distance import squareform
+
+# Add the project root directory to sys.path
+sys.path.append(str(Path(__file__).resolve().parents[1]))
+from streamlining_training.config import (
+    PROCESSED_DATA_DIR, THRESH, DROP_FEATURES,
+)
 
 
-@app.command()
+
 def main(
     # ---- REPLACE DEFAULT PATHS AS APPROPRIATE ----
-    input_path: Path = PROCESSED_DATA_DIR / "dataset.csv",
-    output_path: Path = PROCESSED_DATA_DIR / "features.csv",
+    input_path: Path = PROCESSED_DATA_DIR ,
+    output_path: Path = PROCESSED_DATA_DIR ,
     # -----------------------------------------
 ):
     # ---- REPLACE THIS WITH YOUR OWN CODE ----
     logger.info("Generating features from dataset...")
-    for i in tqdm(range(10), total=10):
-        if i == 5:
-            logger.info("Something happened for iteration 5.")
+
+    
+    
     logger.success("Features generation complete.")
     # -----------------------------------------
 
 
 if __name__ == "__main__":
-    app()
+    main()
